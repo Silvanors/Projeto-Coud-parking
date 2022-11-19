57c729b494c420902ef38c860e31b837cd74bdee
Silvanors <silvanors@gmail.com>
2022-11-19 00:46:29 -0300

    Adicionando swagger

--- src/main/java/one/digitalinnovation/parking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -3,14 +3,18 @@ package one.digitalinnovation.parking.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 //ou
 @RequestMapping("/")
+@ApiIgnore
 public class HelloController {
 
     @GetMapping
     public String hello(){
+
         return "Hello DIO. JAVA DEV...";
     }
+    
 }
